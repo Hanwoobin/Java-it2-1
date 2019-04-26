b2bfa35280faeb06a6184cfb9e157ca5ef5e0d33
USER <USER@AMP-22>
2019-04-26 10:26:43 +0900

    commit

---
+++ Chap09_ex01/src/MyFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame{
+	public MyFrame() {
+			setTitle("asdf");
+			setSize(300,300);
+			setVisible(true);
+	}
+	public static void main(String[] args) {
+		MyFrame fl = new MyFrame();
+
+	}
+
+}
