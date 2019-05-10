81f10ef5a23fe4e8cacc5019e59b278d7d516b5e
USER <USER@AMP-22>
2019-05-10 11:52:13 +0900

    commit

---
+++ chap10_ex5/src/chap10_ex5/ML.java
@@ -0,0 +1,41 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ML extends JFrame {
+	private JLabel la = new JLabel("No I'll not fix your computer");
+
+	public ML() {
+		setTitle("Mouse");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.addMouseListener(new MyMouseListener());
+
+		c.setLayout(null);
+		la.setSize(50, 20);
+		la.setLocation(30, 30);
+		c.add(la);
+
+		setSize(250, 250);
+		setVisible(true);
+	}
+
+	class MyMouseListener implements MouseListener {
+			public void mousePressed(MouseEvent e) {
+				int x = e.getX();
+				int y = e.getY();
+				la.setLocation(x, y);
+		}
+
+		public void mouseReleased(MouseEvent e) {}
+		public void mouseClicked(MouseEvent e){}
+		public void mouseEntered(MouseEvent e){}
+		public void mouseExited(MouseEvent e){}
+	}
+
+	public static void main(String[] args) {
+			new ML();
+	}
+}
+
