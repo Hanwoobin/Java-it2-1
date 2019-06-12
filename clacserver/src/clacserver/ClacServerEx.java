2f68c04d63add4b64887e97d08f0104fbbe4dda8
USER <USER@AMP-22>
2019-06-12 16:03:25 +0900

    commit

---
+++ clacserver/src/clacserver/ClacServerEx.java
@@ -0,0 +1,76 @@
+package clacserver;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.StringTokenizer;
+
+public class ClacServerEx {
+	public static String clac(String exp) {
+		StringTokenizer st = new StringTokenizer(exp, " ");
+		if (st.countTokens() != 3)
+			return "error";
+		String res = "";
+		int op1 = Integer.parseInt(st.nextToken());
+		String opcode = st.nextToken();
+		int op2 = Integer.parseInt(st.nextToken());
+		switch (opcode) {
+		case "+":
+			res = Integer.toString(op1 + op2);
+			break;
+		case "-":
+			res = Integer.toString(op1 - op2);
+			break;
+		case "*":
+			res = Integer.toString(op1 * op2);
+			break;
+		default:
+			res = "error";
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		BufferedReader in = null;
+		BufferedWriter out = null;
+		ServerSocket listener = null;
+		Socket socket = null;
+		try {
+			listener = new ServerSocket(9999);
+			System.out.println("연결을 기다리고있습니다.");
+			socket = listener.accept();
+			System.out.println("연결됨");
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			while (true) {
+				String inputMessage = in.readLine();
+				if (inputMessage.equalsIgnoreCase("bye")) {
+					System.out.println("클라이언트에서 연결을 종료");
+					break;
+				}
+				System.out.println(inputMessage);
+				String res = clac(inputMessage);
+				out.write(res + "\n");
+				out.flush();
+			}
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				if (socket != null)
+					socket.close();
+				if (listener != null)
+					listener.close();
+			} catch (IOException e) {
+				System.out.println("클라이언트와 채팅 중 오류가 발생");
+			}
+		}
+
+	}
+
+}
